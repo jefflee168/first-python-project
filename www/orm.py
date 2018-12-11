325fdc4acc881fefdaf6bb66e2c0ff7a29fab3a4
jefflee168 <86116193@qq.com>
2018-12-11 19:43:51 +0800

    add file www/test1.py

---
+++ www/orm.py
@@ -0,0 +1,47 @@
+import asyncio, logging
+import aiomysql
+
+def log(sql,args=()):
+    logging.info('SQL: %s' % sql)
+
+@asyncio.coroutine
+async def create_pool(loop,**kw):
+    logging.info('create database connection pool ...')
+    global __pool
+    __pool = await aiomysql.create_pool(
+        host=kw.get('host','localhost'),
+        port=kw.get('port',3306),
+        user=kw['user'],
+        password=kw['password'],
+        db=kw['db'],
+        charset=kw.get('charset','utf8'),
+        autocommit=kw.get('autocommit',True),
+        maxsize=kw.get('maxsize',10),
+        minsize=kw.get('minsize',1),
+        loop=loop
+    )
+
+async def select(sql,args,size=None):
+    log(sql,args)
+    global __pool
+    async with __pool.get() as conn:
+        async with conn.cursor(aiomysql.DictCursor) as cur:
+            await cur.execute(sql.replace('?','%s'),args or ())
+            if size:
+                rs = await cur.fetchmany(size)
+            else:
+                rs = await cur.fetchall()
+        logging.info('rows returned:  %s' % len(rs))
+        return rs
+
+async def execute(sql,args):
+    log(sql)
+    with (yield from __pool) as conn:
+        try:
+            cur = yield from conn.cursor()
+            yield from cur.execute(sql.replace('?','%s'),args)
+            affected = cur.rowcount
+            yield from cur.close()
+        except BaseException as e:
+            raise
+        return affected
